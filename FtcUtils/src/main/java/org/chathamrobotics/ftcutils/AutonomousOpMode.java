c9732cb0377aff7ad34611249bbc6f82b9e26121
Carson Storm <stormymcstorm@gmail.com>
2017-06-05 17:46:25 -0400

    cleaned up code base

---
+++ FtcUtils/src/main/java/org/chathamrobotics/ftcutils/AutonomousOpMode.java
@@ -0,0 +1,10 @@
+package org.chathamrobotics.ftcutils;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by storm on 5/26/2017.
+ */
+
+public abstract class AutonomousOpMode extends LinearOpMode {
+}
